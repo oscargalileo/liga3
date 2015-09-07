957ec09641efcda5e3549abc9c9958b0eb0cfd92
Oscar Galileo García García <oscargalileo@gmail.com>
2015-09-06 23:52:14 -0500

    Se agrega un enrutador básico
    Se debería usar un archivo .htaccess con el código sugerido en los
    comentarios.

---
+++ RUTA.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Clase RUTA
+ * Para LIGA 3.x
+ * Autor: Mtro. Oscar Galileo García García
+ * Thanks: http://code.tutsplus.com/tutorials/using-htaccess-files-for-pretty-urls--net-6049
+ * Se recomienda en el .htaccess
+Options -Indexes
+RewriteEngine On
+RewriteCond %{REQUEST_FILENAME} -s [OR]
+RewriteCond %{REQUEST_FILENAME} -l [OR]
+RewriteCond %{REQUEST_FILENAME} -d
+RewriteRule ^.*$ - [NC,L]
+RewriteRule ^.*$ index.php [NC,L]
+ */
+
+class RUTA {
+    static $LPAR = array();
+    static $base  = '';
+    static $error = '<p>Error 404: Dirección inválida.</p>';
+    static $uri   = '';
+    static $rutas = array();
+    private function __construct() {}
+    static function nueva($ruta, $func) {
+        self::$rutas[$ruta] = $func;
+    }
+    static function run() {
+        self::$uri = $_SERVER['REQUEST_URI'];
+        foreach (explode('/', $_SERVER['REQUEST_URI']) as $param) {
+            if ($param) {
+             $param = urldecode($param);
+             if (($pos = strpos($param, '?')) !== false) {
+              $gets = substr($param, $pos + 1);
+              self::$LPAR[] = substr($param, 0, $pos);
+             } else {
+              self::$LPAR[] = $param;
+             }
+            }
+        }
+        if (count(self::$LPAR) > 1) {
+            $lpar = self::$LPAR;
+            unset($lpar[0]);
+            $coincide = false;
+            $ruts = array_keys(self::$rutas);
+            foreach ($ruts as $ruta) {
+                if (!$coincide) {
+                    $rut = explode('/', $ruta);
+                    $coinc = 0;
+                    foreach ($rut as $k => $ru) {
+                        if (isset(self::$LPAR[$k+1]) && ($ru == self::$LPAR[$k+1] || preg_match('/^'.$ru.'$/', self::$LPAR[$k+1]))) {
+                            $coinc++;
+                        } else {
+                            $coinc = 0;
+                            break;
+                        }
+                    }
+                    if (count($lpar) == $coinc) {
+                        $coincide = $ruta;
+                    }
+                }
+            }
+            if ($coincide) {
+                $func = self::$rutas[$coincide];
+                $func();
+                return true;
+            }
+        }
+        if (!headers_sent()) {
+            header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
+        }
+        echo self::$error;
+        return false;
+    }
+}
+?>
