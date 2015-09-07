d272045aca17da4c57ae3bc38fe0f832fdf8f160
Oscar Galileo García García <oscargalileo@gmail.com>
2015-09-07 00:00:46 -0500

    Se reestructura el ejemplo
    Se mejora la estructura para que sea más fácil implementar en proyectos
    nuevos o actualizar los que ya usan versiones antiguas de LIGA.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
  // Cargamos LIGA3
- require_once 'LIGA.php';
+ require_once 'LIGA3/LIGA.php';
+ 
  // Personalizo una conexión a la base de datos (servidor, usuario, contraseña, schema)
  BD('localhost', 'root', '', 'base');
  
  // Configuramos la entidad a usar
- $tabla = 'puestos';
+ $tabla = 'usuarios';
  $liga  = LIGA($tabla);
 
  // Controlador de acciones
@@ -22,49 +23,49 @@
  if (isset($_GET['borrar'])) {
   $resp = $liga->eliminar($_GET['borrar']);
  }
- 
+
  // Si es una petición asíncrona sólo muestro la respuesta
  if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
   echo $resp;
   exit(0);
  }
-  // Imprimo las cabeceras HTML5
+  // Imprimo las etiquetas HTML iniciales
   HTML::cabeceras(array('title'      =>'Pruebas LIGA 3',
 			'description'=>'Página de pruebas para LIGA 3',
-			'css'        =>'LIGA.css',
+			'css'        =>'util/LIGA.css',
 			'style'      =>'label { width:100px; }'
 			)
 		  );
- 
+
  // Guardo el bufer para colocarlo en el layout
  ob_start();
-   // Tabla con instancias
-    $cols = array('*', '-contraseña', 'acción'=>'<a href="?borrar=@[0]">Borrar</a>');
-    $join = array('depende'=>$liga);
-    $pie  = '<th colspan="@[numCols]">Total de instancias: @[numReg]</th>';
-    HTML::tabla($liga, 'Instancias de '.$tabla, $cols, true, $join, $pie);
-    
-    // Formulario para crear nuevas instancias
-    $props  = array('form'=>'method="POST" action="?accion=insertar"',
-		    'input[puesto]'=>array('required'=>'required'));
-    $campos = array('*', '-fecha');
-    HTML::forma($liga, 'Registro de '.$tabla, $campos, $props, true, $_POST);
-    
-    // Formulario para modificar instancias
-    $props  = array('form'=>array('method'=>'POST', 'action'=>'?accion=modificar'), 'prefid'=>'algo',
-		    'input[puesto]'=>array('required'=>'required'));
-    $cual   = !empty($_POST['cual']) ? $_POST['cual'] : '';
-    $select = HTML::selector($liga, 1, array('select'=>array('name'=>'cual', 'id'=>'algocual'),
-							   'option'=>array('value'=>'@[0]'),
-							   "option@si('$cual'=='@[0]')"=>array('selected'=>'selected')), array('depende'=>$liga)
-			     );
-    $campos = array('cual'=>$select, '*', '-fecha');
-    HTML::forma($liga, 'Modificar '.$tabla, $campos, $props, true);
+ // Tabla con instancias
+  $cols = array('*', '-contraseña', 'acción'=>'<a href="?borrar=@[0]">Borrar</a>');
+  $join = array('depende'=>$liga);
+  $pie  = '<th colspan="@[numCols]">Total de instancias: @[numReg]</th>';
+  HTML::tabla($liga, 'Instancias de '.$tabla, $cols, true, $join, $pie);
+  
+  // Formulario para crear nuevas instancias
+  $props  = array('form'=>'method="POST" action="?accion=insertar"',
+		  'input[puesto]'=>array('required'=>'required'));
+  $campos = array('*', '-fecha');
+  HTML::forma($liga, 'Registro de '.$tabla, $campos, $props, true, $_POST);
+  
+  // Formulario para modificar instancias
+  $props  = array('form'=>array('method'=>'POST', 'action'=>'?accion=modificar'), 'prefid'=>'algo',
+		  'input[puesto]'=>array('required'=>'required'));
+  $cual   = !empty($_POST['cual']) ? $_POST['cual'] : '';
+  $select = HTML::selector($liga, 1, array('select'=>array('name'=>'cual', 'id'=>'algocual'),
+							 'option'=>array('value'=>'@[0]'),
+							 "option@si('$cual'=='@[0]')"=>array('selected'=>'selected')), array('depende'=>$liga)
+			   );
+  $campos = array('cual'=>$select, '*', '-fecha');
+  HTML::forma($liga, 'Modificar '.$tabla, $campos, $props, true);
  $cont = ob_get_clean();
  
-  // Cuerpo con layout
+  // Estuctura el cuerpo de la página
   HTML::cuerpo(array('cont'=>$cont));
   
-  // Cierre de etiquetas HTML5
+  // Cierre de etiquetas HTML
   HTML::pie();
 ?>
